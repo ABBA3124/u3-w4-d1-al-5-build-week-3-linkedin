bbd6e5d66485da80693d1808d464fc9377d54696
Carmen Romano <my-9@hotmail.it>
2024-05-15 16:23:20 +0200

    add message component

---
+++ src/components/NavBar/Message.jsx
@@ -0,0 +1,73 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { Accordion, Container } from "react-bootstrap";
+import "./Message.css";
+
+function Message() {
+  const [isOpen, setIsOpen] = useState(false);
+  const profileData = useSelector(state => state.profile.profileData);
+
+  const toggleAccordion = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <Container>
+      <div className="fixed-bottom-right">
+        <Accordion activeKey={isOpen ? "0" : ""}>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header
+              className="accordion-header"
+              onClick={toggleAccordion}
+            >
+              <div className="position-relative mb-2">
+                <img
+                  src={profileData?.image}
+                  alt="img"
+                  width={40}
+                  className="rounded-5 mx-3 mt-3 "
+                />
+                <span className="mx-3 position-absolute top-100 start-50 translate-middle p-2 bg-success border border-light rounded-circle"></span>
+              </div>
+              <p className="mb-1 mx-2 text-dark fw-bold">Messaggistica</p>
+              <i className="bi bi-three-dots mx-2"></i>
+
+              <i className="bi bi-pencil-square mx-2"></i>
+            </Accordion.Header>
+            <Accordion.Body
+              className={`accordion-show p-1 ${isOpen ? "open" : "closed"}`}
+            >
+              <div className="d-flex align-items-center justify-content-between">
+                <div className="d-flex">
+                  <img
+                    src={profileData?.image}
+                    alt="img"
+                    width={40}
+                    className="rounded-2 mx-3 "
+                  />
+                  <p className="mt-2 fw-bold">
+                    {profileData?.name} {profileData?.surname}
+                  </p>
+                </div>
+                <div>
+                  <i className="bi bi-arrow-right-short fs-2"></i>
+                </div>
+              </div>
+              <div className="mx-3 mt-2">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </div>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </div>
+    </Container>
+  );
+}
+
+export default Message;
