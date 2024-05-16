f6cb5615149360b0fe990375764adac05d2b7e6f
Carmen Romano <my-9@hotmail.it>
2024-05-16 16:42:25 +0200

    job

--- src/components/AllComponents/HomePageMain.jsx
+++ src/components/AllComponents/HomePageMain.jsx
@@ -1,13 +1,13 @@
-import MyNavbar from "../NavBar/MyNavbar"
-import MainProfile from "../MainProfile/mainprofile"
-import MyFooter from "../Footer/MyFooter"
-import "../AllComponents/HomePageMain.css"
-import ProfileList from "../ProfileList/ProfileList"
-import HomePage from "../HomePage/HomePage"
-import Message from "../NavBar/Message"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Job from "../Job/Job"
-import MainProfileEAsideBar from "../AllComponents/MainProfileEAsideBar"
+import MyNavbar from "../NavBar/MyNavbar";
+import MyFooter from "../Footer/MyFooter";
+import "../AllComponents/HomePageMain.css";
+import ProfileList from "../ProfileList/ProfileList";
+import HomePage from "../HomePage/HomePage";
+import Message from "../NavBar/Message";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Job from "../Job/Job";
+import MainProfileEAsideBar from "../AllComponents/MainProfileEAsideBar";
+import JobSearch from "../Job/JobSearch";
 
 const HomePageMain = () => {
   return (
@@ -22,13 +22,14 @@ const HomePageMain = () => {
               <Route path="/Profile" element={<MainProfileEAsideBar />} />
               <Route path="/Search" element={<ProfileList />} />
               <Route path="/job" element={<Job />} />
+              <Route path="/job/search" element={<JobSearch />} />
             </Routes>
           </div>
         </div>
         <MyFooter />
       </div>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default HomePageMain
+export default HomePageMain;
