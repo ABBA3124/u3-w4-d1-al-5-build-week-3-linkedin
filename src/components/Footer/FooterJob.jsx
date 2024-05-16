bc8fa8dcd2b6a7624afbf481d6389faaa44c90ff
ABBA3124 <davideabbadessa73@gmail.com>
2024-05-16 13:37:43 +0200

    fix footer jobs
    fix

---
+++ src/components/Footer/FooterJob.jsx
@@ -0,0 +1,203 @@
+import React from "react"
+import { Button, Col, Dropdown, DropdownButton, Form, Image, Modal, Nav, Row } from "react-bootstrap"
+
+const FooterJob = (props) => {
+  const [modalShow, setModalShow] = React.useState(false)
+  return (
+    <div className="text-center">
+      <Row className="row-cols-1 row-cols-sm-2 row-cols-md-2 py-2">
+        <Col className="my-1 fs-6">
+          <Nav.Link href="#" className="link">
+            Informazioni
+          </Nav.Link>
+        </Col>
+        <Col className="my-1 fs-6">
+          <Nav.Link href="#" className="link">
+            Accessibilità
+          </Nav.Link>
+        </Col>
+        <Col className="my-1 fs-6">
+          <Nav.Link href="#" className="link pb-0">
+            Centro Assistenza
+          </Nav.Link>
+        </Col>
+        <Col className="my-1 fs-6">
+          <DropdownButton id="dropdown-button" title="Privacy e condizioni" variant="transparent" className="py-0">
+            <Dropdown.Item href="#/action-1">Informazioni sulla privacy</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Contratto di licenza</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Termini e condizioni delle pagine</Dropdown.Item>
+            <Dropdown.Item href="#/action-4">Informativa sui cookie</Dropdown.Item>
+            <Dropdown.Item href="#/action-5">Informativa sul copyright</Dropdown.Item>
+          </DropdownButton>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="my-1 fs-6">
+          <Nav.Link href="#" className="link">
+            Opzione per gli annunci pubblicitari
+          </Nav.Link>
+        </Col>
+      </Row>
+      <Row className="row-cols-1 row-cols-sm-2 row-cols-md-2 py-2">
+        <Col className="my-1 fs-6">
+          <Nav.Link href="#" className="link">
+            Pubblicità
+          </Nav.Link>
+        </Col>
+        <Col className="my-1 fs-6">
+          <DropdownButton id="dropdown-button" title="Servizi alle aziende" variant="transparent" className="py-0">
+            <Dropdown.Item href="#/action-1">Informazioni sulla privacy</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Contratto di licenza</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Termini e condizioni delle pagine</Dropdown.Item>
+            <Dropdown.Item href="#/action-4">Informativa sui cookie</Dropdown.Item>
+            <Dropdown.Item href="#/action-5">Informativa sul copyright</Dropdown.Item>
+          </DropdownButton>
+        </Col>
+
+        <Col className="my-1 fs-6">
+          <Nav.Link href="#" className="link">
+            Scarica l app LinkedIn
+          </Nav.Link>
+        </Col>
+        <Col className="my-1 fs-6">
+          <Button variant="transparent" onClick={() => setModalShow(true)}>
+            Altro
+          </Button>
+
+          <Modal
+            {...props}
+            size="xl"
+            aria-labelledby="contained-modal-title-vcenter"
+            show={modalShow}
+            onHide={() => setModalShow(false)}
+            dialogClassName="custom-modal"
+          >
+            <Modal.Header closeButton></Modal.Header>
+            <Modal.Body>
+              <Row className="me-auto">
+                <Col sm={8} className="d-flex">
+                  <Row className="row-cols-1 row-cols-sm-2 row-cols-md-3 py-2">
+                    <Col className="my-1 fs-6">
+                      <Nav.Link href="#" className="link">
+                        Informazioni
+                      </Nav.Link>
+                    </Col>
+                    <Col className="my-1 fs-6">
+                      <Nav.Link href="#" className="link">
+                        Accessibilità
+                      </Nav.Link>
+                    </Col>
+                    <Col className="my-1 fs-6">
+                      <Nav.Link href="#" className="link">
+                        Talent Solutions
+                      </Nav.Link>
+                    </Col>
+
+                    <Col className="my-1 fs-6">
+                      <Nav.Link href="#" className="link">
+                        Linee guida della community
+                      </Nav.Link>
+                    </Col>
+                    <Col className="my-1 fs-6">
+                      <Nav.Link href="#" className="link">
+                        Carriera
+                      </Nav.Link>
+                    </Col>
+                    <Col className="my-1 fs-6">
+                      <Nav.Link href="#" className="link">
+                        Soluzione di marketing
+                      </Nav.Link>
+                    </Col>
+
+                    <Col className="my-1 fs-6">
+                      <DropdownButton id="dropdown-button" title="Privacy e condizioni" variant="transparent">
+                        <Dropdown.Item href="#/action-1">Informazioni sulla privacy</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">Contratto di licenza</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">Termini e condizioni delle pagine</Dropdown.Item>
+                        <Dropdown.Item href="#/action-4">Informativa sui cookie</Dropdown.Item>
+                        <Dropdown.Item href="#/action-5">Informativa sul copyright</Dropdown.Item>
+                      </DropdownButton>
+                    </Col>
+                    <Col className="my-1 fs-6">
+                      <Nav.Link href="#" className="link">
+                        Opzioni per gli annunci pubblicitari
+                      </Nav.Link>
+                    </Col>
+                    <Col className="my-1 fs-6">
+                      <Nav.Link href="#" className="link">
+                        Pubblicità
+                      </Nav.Link>
+                    </Col>
+
+                    <Col className="my-1 fs-6">
+                      <Nav.Link href="#" className="link">
+                        Sales solutions
+                      </Nav.Link>
+                    </Col>
+                    <Col className="my-1 fs-6">
+                      <Nav.Link href="#" className="link">
+                        Mobile
+                      </Nav.Link>
+                    </Col>
+                    <Col className="my-1 fs-6">
+                      <Nav.Link href="#" className="link">
+                        Piccole imprese
+                      </Nav.Link>
+                    </Col>
+
+                    <Col className="mb-2">
+                      <Nav.Link href="#" className="link">
+                        Centro sicurezza
+                      </Nav.Link>
+                    </Col>
+                  </Row>
+                  <Row className="row-cols-1 py-3" style={{ whiteSpace: "nowrap" }}>
+                    <Col>
+                      <div className="d-flex justify-content-start fs-5 py-2">
+                        <i className="bi bi-question-circle-fill"></i>
+                        <Nav.Link href="#" className="link px-1">
+                          Domande?
+                        </Nav.Link>
+                      </div>
+                      <div className="d-flex justify-content-start fs-5 py-2">
+                        <i className="bi bi-gear-fill"></i>
+                        <Nav.Link href="#" className="link px-1">
+                          Gestisci il tuo account o la tua privacy
+                        </Nav.Link>
+                      </div>
+                      <div className="d-flex justify-content-start fs-5 py-2">
+                        <i className="bi bi-shield-shaded"></i>
+                        <Nav.Link href="#" className="link px-1">
+                          Trasparenza sui contenuti consigliati
+                        </Nav.Link>
+                      </div>
+                    </Col>
+                  </Row>
+                </Col>
+                <Col sm={4} className="px-5">
+                  <p className="fs-6 mb-1 mt-4 text-secondary">Seleziona lingua</p>
+                  <Form.Select className="border-black text-secondary">
+                    <option>Italiano (italiano)</option>
+                    <option>English (english)</option>
+                    <option>French (french)</option>
+                    <option>Indian (indian)</option>
+                  </Form.Select>
+                  <br />
+                </Col>
+                <Row>
+                  <Col className="mb-2 mt-2 copyright text-secondary">Linkedin Corporation © 2024</Col>
+                </Row>
+              </Row>
+            </Modal.Body>
+            <Modal.Footer></Modal.Footer>
+          </Modal>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="my-1 fs-6 copyright text-secondary">Linkedin Corporation © 2024</Col>
+      </Row>
+    </div>
+  )
+}
+
+export default FooterJob
--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -1,153 +1,129 @@
-import React, { useEffect, useState } from "react";
-import { Button, Col, Container, Row } from "react-bootstrap";
-import "./Job.css";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from "react"
+import { Button, Col, Container, Row } from "react-bootstrap"
+import "./Job.css"
+import { useSelector } from "react-redux"
+import FooterJob from "../Footer/FooterJob"
 
 const Job = () => {
-  const [jobs, setJobs] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const profileData = useSelector(state => state.profile.profileData);
+  const [jobs, setJobs] = useState([])
+  const [loading, setLoading] = useState(false)
+  const profileData = useSelector((state) => state.profile.profileData)
 
-  const fetchJobs = async url => {
-    setLoading(true);
+  const fetchJobs = async (url) => {
+    setLoading(true)
     try {
-      const response = await fetch(url);
-      const allJobs = await response.json();
-      setJobs(allJobs.data);
+      const response = await fetch(url)
+      const allJobs = await response.json()
+      setJobs(allJobs.data)
     } catch (error) {
-      console.error("Errore fetch jobs:", error);
+      console.error("Errore fetch jobs:", error)
     } finally {
-      setLoading(false);
+      setLoading(false)
     }
-  };
+  }
 
-  const searchJobs = query => {
-    fetchJobs(
-      `https://strive-benchmark.herokuapp.com/api/jobs?search=${query}`
-    );
-  };
+  const searchJobs = (query) => {
+    fetchJobs(`https://strive-benchmark.herokuapp.com/api/jobs?search=${query}`)
+  }
 
-  const searchByCompany = company => {
-    fetchJobs(
-      `https://strive-benchmark.herokuapp.com/api/jobs?company=${company}`
-    );
-  };
+  const searchByCompany = (company) => {
+    fetchJobs(`https://strive-benchmark.herokuapp.com/api/jobs?company=${company}`)
+  }
 
-  const searchByCategory = category => {
-    fetchJobs(
-      `https://strive-benchmark.herokuapp.com/api/jobs?category=${category}&limit=10`
-    );
-  };
+  const searchByCategory = (category) => {
+    fetchJobs(`https://strive-benchmark.herokuapp.com/api/jobs?category=${category}&limit=10`)
+  }
 
   useEffect(() => {
-    searchByCategory("develop");
-  }, []);
+    searchByCategory("develop")
+  }, [])
 
   return (
     <>
       <Container>
         <Row className="p-4">
-          <Col md={4}>
-            <div className="rounded-4 p-4 bg-light text-start job">
-              <div className="d-flex align-items-center flex-row ">
-                <i className="bi bi-bookmark-fill me-2"></i>
-                <span className="fw-bold mx-1 my-1">
-                  Le mie offerte di lavoro
-                </span>
-              </div>
-              <div className="d-flex align-items-center flex-row">
-                <i className="bi bi-list-ul me-2"></i>
-                <span className="fw-bold mx-1 my-1">Preferenze</span>
-              </div>
-              <div className="d-flex align-items-center flex-row">
-                <i className="bi bi-clipboard2-check me-2"></i>
-                <span className="fw-bold mx-1 my-1">
-                  Valutazione delle competenze
-                </span>
-              </div>
-              <div className="d-flex align-items-center flex-row">
-                <i className="bi bi-youtube me-2"></i>
-                <span className="fw-bold mx-1 my-1">
-                  Indicazioni per chi cerca lavoro
-                </span>
-              </div>
-            </div>
+          <Col md={9}>
+            <Row className="p-4">
+              <Col md={4}>
+                <div className="rounded-4 p-4 bg-light text-start job">
+                  <div className="d-flex align-items-center flex-row ">
+                    <i className="bi bi-bookmark-fill me-2"></i>
+                    <span className="fw-bold mx-1 my-1">Le mie offerte di lavoro</span>
+                  </div>
+                  <div className="d-flex align-items-center flex-row">
+                    <i className="bi bi-list-ul me-2"></i>
+                    <span className="fw-bold mx-1 my-1">Preferenze</span>
+                  </div>
+                  <div className="d-flex align-items-center flex-row">
+                    <i className="bi bi-clipboard2-check me-2"></i>
+                    <span className="fw-bold mx-1 my-1">Valutazione delle competenze</span>
+                  </div>
+                  <div className="d-flex align-items-center flex-row">
+                    <i className="bi bi-youtube me-2"></i>
+                    <span className="fw-bold mx-1 my-1">Indicazioni per chi cerca lavoro</span>
+                  </div>
+                </div>
+              </Col>
+
+              <Col md={8}>
+                <div className="rounded-4 p-4 bg-light text-start">
+                  <p className="fw-bold fs-5">Le principali offerte di lavoro per te</p>
+                  <p className="text-secondary">Sulla base del tuo profilo e della tua cronologia delle ricerche</p>
+                  {jobs.slice(0, 4).map((job) => (
+                    <div className="text-align-start mb-3" key={job._id}>
+                      <p className="fw-bold text-primary">{job.title}</p>
+                      <p>{job.company_name}</p>
+                      <p className="text-secondary">{job.candidate_required_location}</p>
+                      <hr />
+                    </div>
+                  ))}
+                </div>
+                <div className="rounded-4 p-4 bg-light text-start mt-3 premium-job">
+                  <p className=" mb-0 title-job">Offerte di lavoro per cui rientri fra i migliori candidati</p>
+                  <p className="text-secondary fs-6 mt-0 title-job-2">
+                    In base alle tue probabilità di ricevere una risposta
+                  </p>
+
+                  <div className="d-flex align-items-start premium-job-2">
+                    <img src={profileData?.image} alt="img" width={50} className="rounded-5 mt-2 mx-4" />
+
+                    <div>
+                      <span className="fw-bold">
+                        Vedi l&apos;elenco completo delle offerte di lavoro per cui saresti fra i migliori candidati
+                      </span>
+                      <p className="text-secondary">{profileData?.name} e milioni di altri utenti usano Premium</p>
+                      <Button className="bg-warning rounded-5 text-dark fw-bold border-0 mb-3">
+                        Prova Premium per 0 EUR
+                      </Button>
+                      <p className="text-secondary">
+                        Prova gratuita di 1 mese. Ti invieremo un promemoria 7 giorni prima della fine del periodo di
+                        prova.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="rounded-4 p-4 bg-light text-start mt-3">
+                  <p className="fw-bold fs-5">Altre offerte di lavoro per te</p>
+                  <p className="text-secondary">Sulla base del tuo profilo e della tua cronologia delle ricerche</p>
+                  {jobs.slice(0, 3).map((job) => (
+                    <div className="text-align-start mb-2" key={job._id}>
+                      <p className="fw-bold text-primary">{job.title}</p>
+                      <p>{job.company_name}</p>
+                      <p className="text-secondary">{job.candidate_required_location}</p>
+                      <hr />
+                    </div>
+                  ))}
+                </div>
+              </Col>
+            </Row>
           </Col>
-
-          <Col md={8}>
-            <div className="rounded-4 p-4 bg-light text-start">
-              <p className="fw-bold fs-5">
-                Le principali offerte di lavoro per te
-              </p>
-              <p className="text-secondary">
-                Sulla base del tuo profilo e della tua cronologia delle ricerche
-              </p>
-              {jobs.slice(0, 4).map(job => (
-                <div className="text-align-start mb-3" key={job._id}>
-                  <p className="fw-bold text-primary">{job.title}</p>
-                  <p>{job.company_name}</p>
-                  <p className="text-secondary">
-                    {job.candidate_required_location}
-                  </p>
-                  <hr />
-                </div>
-              ))}
-            </div>
-            <div className="rounded-4 p-4 bg-light text-start mt-3 premium-job">
-              <p className=" mb-0 title-job">
-                Offerte di lavoro per cui rientri fra i migliori candidati
-              </p>
-              <p className="text-secondary fs-6 mt-0 title-job-2">
-                In base alle tue probabilità di ricevere una risposta
-              </p>
-
-              <div className="d-flex align-items-start premium-job-2">
-                <img
-                  src={profileData?.image}
-                  alt="img"
-                  width={50}
-                  className="rounded-5 mt-2 mx-4"
-                />
-
-                <div>
-                  <span className="fw-bold">
-                    Vedi l&apos;elenco completo delle offerte di lavoro per cui
-                    saresti fra i migliori candidati
-                  </span>
-                  <p className="text-secondary">
-                    {profileData?.name} e milioni di altri utenti usano Premium
-                  </p>
-                  <Button className="bg-warning rounded-5 text-dark fw-bold border-0 mb-3">
-                    Prova Premium per 0 EUR
-                  </Button>
-                  <p className="text-secondary">
-                    Prova gratuita di 1 mese. Ti invieremo un promemoria 7
-                    giorni prima della fine del periodo di prova.
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="rounded-4 p-4 bg-light text-start mt-3">
-              <p className="fw-bold fs-5">Altre offerte di lavoro per te</p>
-              <p className="text-secondary">
-                Sulla base del tuo profilo e della tua cronologia delle ricerche
-              </p>
-              {jobs.slice(0, 3).map(job => (
-                <div className="text-align-start mb-2" key={job._id}>
-                  <p className="fw-bold text-primary">{job.title}</p>
-                  <p>{job.company_name}</p>
-                  <p className="text-secondary">
-                    {job.candidate_required_location}
-                  </p>
-                  <hr />
-                </div>
-              ))}
-            </div>
+          <Col md={3}>
+            <FooterJob />
           </Col>
         </Row>
       </Container>
     </>
-  );
-};
+  )
+}
 
-export default Job;
+export default Job
